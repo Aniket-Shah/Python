8c4572a4105a5422b32863b5a54e78e3fffa79ca
Aniket-Shah <shahaniket2000@gmail.com>
2024-03-24 18:51:02 +0530

    Learning List, Tuple, Set, and Dictionary in Python

---
+++ pratices/listTupleSetDictionary.py
@@ -0,0 +1,45 @@
+# List
+l1=[3,5,234,234,234]
+
+# print(type (l1))
+# print(l1)
+# print(l1.count(234))
+# l1.sort()
+# l1.pop()
+# l1.append(24)
+# l1.clear()
+# l1.extend([4567,7654,232,3112,312])
+# print(l1)
+
+# Tuple
+t1 = (1,2,3,4,5,6,7,8,9)
+# print(t1.count(5))
+# print(t1.index(8))
+
+# different between list and tuple is : list can be changed were as tuple is immutable
+
+# Set
+
+a = {3,5,23,4,32,1,4}
+b = {2344,1232,345,13123,45}
+# print (a.union(b))
+# print (a.intersection(b))
+# print (a)
+
+
+# Dictionary is mutable
+# a = {}
+# b = set()
+# print (type(a))
+# print (type(b))
+
+
+dict1={"good": "Something pleasant", "bad": "Something wrong", "fetch": "Get me Something"}
+
+marks = {"Aniket": 90, "Bob": 80, "David":60}
+
+print (dict1["good"])
+print (marks.keys())
+
+marks["Priya"] = 8
+print (marks)
