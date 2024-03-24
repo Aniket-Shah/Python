e6daa48c68c4f7a89f2c434ed1d02d19efff55e7
Aniket-Shah <shahaniket2000@gmail.com>
2024-03-24 13:14:53 +0530

    Learning String and String Method in Python

---
+++ pratices/stringMethod.py
@@ -0,0 +1,19 @@
+name = "Aniket"
+print(name)
+
+print (name[0:3])
+print (name[1:5])
+print (name[0:])
+print (name[:-1])
+print(name.upper())
+print(name.lower())
+print (name.isalnum())
+
+
+
+#####User Input
+name = input ("Enter your name : ")
+print(name)
+## in Input function the variable is default in string
+
+
