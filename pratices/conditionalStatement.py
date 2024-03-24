1919ddb1b8be6caccbfdc9f2f36d9800118ea277
Aniket-Shah <shahaniket2000@gmail.com>
2024-03-24 19:04:15 +0530

    Learning List, Tuple, Set, and Dictionary in Python

---
+++ pratices/conditionalStatement.py
@@ -0,0 +1,21 @@
+# age = int(input("Enter your age: "))
+#
+# if (age>=18):
+#     print ("Yes, you can drive")
+# elif (age<18):
+#     print ("You are a kid")
+# else:
+#     print("No, you can go home")
+
+a =4
+match a:
+    case 1:
+        print("Case is 1")
+    case 2:
+        print("Case is 2")
+    case 3:
+        print("Case is 3")
+    case 4:
+        print("Case is 4")
+    case _:
+        print("No Match Found")
---
+++ pratices/loops.py
@@ -0,0 +1,11 @@
+# for i in range(1,11):
+#     print (i)
+#
+# a = [1,3,234,6543,7789,987]
+# for item in a:
+#     print (item)
+
+i=0
+while(i<10):
+    print (i)
+    i+=1
