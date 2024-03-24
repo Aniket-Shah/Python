e293f0531185eb777045479b061af7efec1d7a69
Aniket-Shah <shahaniket2000@gmail.com>
2024-03-24 12:42:33 +0530

    Learning Commits,TypeCasting and Variable in Python

--- pratices/helloPython.py
+++ pratices/helloPython.py
@@ -11,3 +11,18 @@ print("hello world")
 # print("hello world")
 
 #Variable in Python
+a = 3
+print(a)
+b=55
+print(b)
+a=6.2
+print(a)
+print(type(a))
+
+
+#TypeCasting
+f="5"
+print(int(f)+1)
+g=5
+print(g)
+
