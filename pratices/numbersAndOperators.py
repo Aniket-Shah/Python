02258ab4084d48516a8195b14bcc6c4984dafa68
Aniket-Shah <shahaniket2000@gmail.com>
2024-03-24 12:56:50 +0530

    Learning number and Operators in Python

---
+++ pratices/numbersAndOperators.py
@@ -0,0 +1,36 @@
+num1 = 10
+num2 =2
+
+#Arithmetic Operator
+print("Add =",num1+num2)
+print("Sub =",num1-num2)
+print("Mul =",num1 * num2)
+print("Div =",num1 / num2)
+print("Float Div =",num1 // num2)
+print("Power Mul =",num1 ** num2)
+print("Mod =",num1 % num2)
+
+# Assign ment Operator
+a=4
+a+=2
+print (a)
+a-=2
+print (a)
+a*=2
+print (a)
+a/=2
+print (a)
+
+#Comparison Opertors
+x=3
+y=8
+z=3
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==z)
+
+#Logical Operators
+print(x==z and x > y)
+print(x==z and x < y)
+print(not(x==z))
